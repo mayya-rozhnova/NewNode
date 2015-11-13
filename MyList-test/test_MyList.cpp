27bd229520b06c357201ae50d3a866d07fe75d2c
mayya-rozhnova <wooownn@gmail.com>
2015-11-13 19:59:36 +0300

    Больше тестов...

--- MyList-test/test_MyList.cpp
+++ MyList-test/test_MyList.cpp
@@ -23,7 +23,39 @@ TEST(MyList, CreateList) {
   EXPECT_EQ(kListSize, k);
 }
 
-TEST(MyList, IsCycle) {
+TEST(MyList, NoCycle_in_empty) {
+	CNode *Head = NULL;
+	bool f = Cycle(Head);
+	EXPECT_EQ(false, f);
+}
+
+TEST(MyList, NoCycle) {
+	int a[5] = {2,4,6,8,10};
+
+	CNode* Head = CreateList(5, a);
+	bool f = Cycle(Head);
+	EXPECT_EQ(false, f);
+}
+
+TEST(MyList, IsCycle_one_without_cycle) {
+	CNode *P = new CNode;
+	CNode *Head = P;
+	P->val = 1;
+	P->next = NULL;
+	bool f = Cycle(Head);
+	EXPECT_EQ(false, f);
+}
+
+TEST(MyList, IsCycle_one_with_cycle) {
+	CNode *P = new CNode;
+	CNode *Head = P;
+	P->val = 1;
+	P->next = P;
+	bool f = Cycle(Head);
+	EXPECT_EQ(true, f);
+}
+
+TEST(MyList, IsCycle_cycle_first_element) {
 	CNode *P=new CNode;
 	CNode *Head=P;
 	P->val = 1;
@@ -39,10 +71,58 @@ TEST(MyList, IsCycle) {
 	EXPECT_EQ (true, f);
 }
 
-TEST(Mylist, NoCycle) {
-	int a[5] = {2,4,6,8,10};
-
-	CNode* Head = CreateList(5, a);
+TEST(MyList, Is_Cycle_cycle_second_element) {
+	CNode *P=new CNode;
+	CNode *Head=P;
+	P->val = 1;
+	CNode *Tmp2 = new CNode;
+	for (int i = 1; i < 4; i++)
+	{
+		CNode *Tmp = new CNode;
+		Tmp->val = i + 1;
+		P->next = Tmp;
+		P = P->next;
+		if (i == 1)
+		Tmp2 = P;
+	}
+	P->next = Tmp2;
 	bool f = Cycle(Head);
-	EXPECT_EQ(false, f);
+	EXPECT_EQ (true, f);
 }
+
+TEST(MyList, Is_Cycle_cycle_third_element) {
+	CNode *P=new CNode;
+	CNode *Head=P;
+	P->val = 1;
+	CNode *Tmp2 = new CNode;
+	for (int i = 1; i < 4; i++)
+	{
+		CNode *Tmp = new CNode;
+		Tmp->val = i + 1;
+		P->next = Tmp;
+		P = P->next;
+		if (i == 2)
+		Tmp2 = P;
+	}
+	P->next = Tmp2;
+	bool f = Cycle(Head);
+	EXPECT_EQ (true, f);
+}
+
+TEST(MyList, Is_Cycle_cycle_last_element) {
+	CNode *P=new CNode;
+	CNode *Head=P;
+	P->val = 1;
+	CNode *Tmp2 = new CNode;
+	for (int i = 1; i < 4; i++)
+	{
+		CNode *Tmp = new CNode;
+		Tmp->val = i + 1;
+		P->next = Tmp;
+		P = P->next;
+	}
+	P->next = P;
+	bool f = Cycle(Head);
+	EXPECT_EQ (true, f);
+}
+
